64900108a231d3b2bb5c8f2737806d0cd5d005e3
Joe Burgess <bojurgess@gmail.com>
2026-04-11 20:03:09 +0100

    define api

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[wasm_bindgen(js_name = extractPalette)]
+pub fn extract_palette(data: &[u8]) -> Result<JsValue, JsError> {
+    todo!()
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, colorquant!");
+#[wasm_bindgen(js_name = medianCut)]
+pub fn median_cut(rgba: &[u8], k: u32) -> Result<JsValue, JsError> {
+    todo!()
+}
+
+#[wasm_bindgen(js_name = octreeQuantize)]
+pub fn octree_quantize(rgba: &[u8], k: u32) -> Result<JsValue, JsError> {
+    todo!()
 }
