69b7e871db113ed2e40af8545745bd047ec2835d
Joe Burgess <bojurgess@gmail.com>
2026-06-08 23:05:34 +0100

    hopefully fix publish workflow

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 for dir in pkg/bundler pkg/web pkg/node; do
-    rm -f "$dir/package.json" "$dir/.gitignore" "$dir/README.md"
+    rm -f "$dir/package.json" "$dir/.gitignore" "$dir/README.md" "$dir/LICENSE"
 done
+cp LICENSE README.md pkg/
